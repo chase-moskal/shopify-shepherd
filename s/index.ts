0f6c61a60a893ee58ef1e8cc3f503f9f686b8d55
Chase Moskal <chasemoskal@gmail.com>
2023-07-03 18:14:13 -0700

    trim: package exports

--- s/index.ts
+++ s/index.ts
@@ -1,23 +1,21 @@
 
 export * from "./shopify.js"
 
-export * from "./parts/pagination/paginate.js"
-
 export * from "./parts/remote/remote.js"
 export * from "./parts/remote/types/shopify_settings.js"
-export * from "./parts/remote/defaults/default_api_version.js"
-export * from "./parts/remote/defaults/default_page_size.js"
 
-export * from "./requests/types/graph_request.js"
-export * from "./requests/units/edges.js"
-export * from "./requests/units/image.js"
-export * from "./requests/units/paginated.js"
-export * from "./requests/units/variants.js"
+export type * from "./requests/types/graph_request.js"
+export type * from "./requests/units/edges.js"
+export type * from "./requests/units/image.js"
+export type * from "./requests/units/paginated.js"
+export type * from "./requests/units/product.js"
+export type * from "./requests/units/variants.js"
 
-export * from "./requests/collections.js"
-export * from "./requests/products.js"
-export * from "./requests/shop.js"
+export type * from "./requests/collections.js"
+export type * from "./requests/product_recommendations.js"
+export type * from "./requests/products.js"
+export type * from "./requests/products_in_collection.js"
+export type * from "./requests/shop.js"
 
 export * from "./utils/errors.js"
-export * from "./utils/gql.js"
 
